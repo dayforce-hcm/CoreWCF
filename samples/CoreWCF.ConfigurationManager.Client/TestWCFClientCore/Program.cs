0853238770cd74cc6ed9f4e3a6524d000bead3be
alex-yakimov <alexander.yakimov.public@dayforce.com>
2026-02-09 18:47:34 -0500

    3. Added client and service migrated from net472 to net8

---
+++ samples/CoreWCF.ConfigurationManager.Client/TestWCFClientCore/Program.cs
@@ -0,0 +1,85 @@
+﻿using CoreWCF.ConfigurationManager.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestWCFClientLib.BL;
+using TestWCFClientLib.WeatherService;
+
+namespace TestWCFClientCoreApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Reference interface to load Library into the CurrentDomain asseblies list
+            IWeatherService weatherService = null;
+            var serviceProvider = GetServiceProvider();
+            var endpointBuilder = serviceProvider.GetService<IServiceEndpointBuilder>();
+            var endpoint = endpointBuilder.ConstructServiceEndPoint("NetCore_BasicHttpBinding_IWeatherService");
+
+            await WrapperBaseUsage(endpoint);
+            await DirectClientUsage(endpoint);
+
+        } 
+        
+        public static ServiceProvider GetServiceProvider()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .Build();
+
+            var provider = new ServiceCollection()
+                .AddSingleton(configuration)
+                .AddClientModelServices()
+                .AddClientModelConfigurationManagerFile(Path.Combine(AppContext.BaseDirectory, "app.config"))                             
+                .BuildServiceProvider();
+            
+            return provider;
+        }
+
+        private static async Task WrapperBaseUsage(System.ServiceModel.Description.ServiceEndpoint endpoint)
+        {
+            using (var weatherServiceWrapper = new WeatherServiceWrapperBase(endpoint))
+            {
+                var results = await weatherServiceWrapper.GetWeatherforecastAsync();
+                PrintResults(results);
+                var updateResult = await weatherServiceWrapper.UpdateForecastAsync(
+                    new WeatherForecast
+                    {
+                        Date = DateTime.Now.ToString(),
+                        TemperatureC = 10,
+                        Summary = "Sunny"
+                    });
+                PrintResult($"{updateResult}");
+            }
+        }
+
+        private static async Task DirectClientUsage(System.ServiceModel.Description.ServiceEndpoint endpoint)
+        {
+            var weatherServiceClient = new WeatherServiceClient(endpoint.Binding, endpoint.Address);
+
+            var results = await weatherServiceClient.GetWeatherforecastAsync();
+            PrintResults(results);
+            var updateResult = await weatherServiceClient.UpdateForecastAsync(
+                new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(2)).ToShortDateString(),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = "Cloudy"
+                });
+            PrintResult($"{updateResult}");
+        }
+
+        private static void PrintResults(IEnumerable<WeatherForecast> weatherForecasts)
+        {
+            foreach (WeatherForecast forecast in weatherForecasts)
+            {
+                PrintResult($"Date: {forecast.Date}, TemperatureC: {forecast.TemperatureC}, Summary: {forecast.Summary}");
+            }
+        }
+
+        private static void PrintResult(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ samples/CoreWCF.ConfigurationManager.Client/TestWCFServiceCore/Startup.cs
@@ -0,0 +1,59 @@
+﻿using CoreWCF.Configuration;
+using CoreWCF.Description;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+//using TestWCFServiceCore.Service;
+using TestWCFServiceLib.Service;
+
+namespace TestWCFServiceCore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServiceModelServices()
+                .AddServiceModelMetadata()
+                .AddAuthorization()
+                .AddServiceModelConfigurationManagerFile(Path.Combine(AppContext.BaseDirectory, "WeatherService.config"));
+
+            services.AddTransient<WeatherService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.UseServiceModel(builder =>
+            {
+                builder.AddService<WeatherService>();
+
+                var serviceMetadataBehavior = app.ApplicationServices.GetRequiredService<ServiceMetadataBehavior>();
+                serviceMetadataBehavior.HttpGetEnabled = serviceMetadataBehavior.HttpsGetEnabled = true;
+            });
+        }
+    }
+}
